bb0a1774e3eb175fca8f86d06585be514819fb46
AceLi <fx.yoyo.goldensun@gmail.com>
2018-03-16 15:07:56 +0800

    Add 64.

---
+++ LeetCode/64. Minimum Path Sum.cpp
@@ -0,0 +1,27 @@
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+// Note: You can only move either down or right at any point in time.
+
+// Example 1:
+// [[1,3,1],
+//  [1,5,1],
+//  [4,2,1]]
+// Given the above grid map, return 7. Because the path 1→3→1→1→1 minimizes the sum.
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<int> d(n, 0);
+        vector<vector<int>> dp(m, d);
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++)
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for(int i = 1; i < n; i++)
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++)
+                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+        return dp[m - 1][n - 1];
+    }
+};
